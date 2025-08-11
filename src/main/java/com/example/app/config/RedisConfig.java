a37475368500141942d3c5fe96c01f39042b3a13
pkudle <pkudle@gmail.com>
2025-08-11 21:09:56 +0530

    added redis config explicitly

---
+++ src/main/java/com/example/app/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.example.app.config;
+
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("master.my-redis-v1.mkpp9f.aps1.cache.amazonaws.com");
+        config.setPort(6582);
+
+        // AWS Redis with TLS
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl()
+                .commandTimeout(Duration.ofSeconds(60))
+                .build();
+
+        return new LettuceConnectionFactory(config, clientConfig);
+    }
+}
